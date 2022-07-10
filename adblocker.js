6adb80c513ee9ced4dbe77298f55402f93e0208e
Zombie <93808177+RpSide@users.noreply.github.com>
2022-07-10 01:44:29 -0500

    Add files via upload

---
+++ adblocker.js
@@ -0,0 +1,25 @@
+// Official Gateway Ad Blocker
+for(let e=0;e<40;e++){
+    lads=["sc-1i7jkv7-2","sc-1lqea4y-0"
+    , "ytd-display-ad-renderer"
+    , "ytd-promoted-sparkles-web-renderer"
+    , "ytp-ad-image ytp-ad-action-interstitial-background"
+    , "ytp-ad-module", "GoogleCreativeContainerClass"
+    , "ytp-ad-image-overlay"
+    , "ytp-ad-overlay-closed"
+    , "ytd-action-companion-ad-renderer"
+    , "ytd-action-companion-ad-renderer"
+    , "ytd-action-companion-ad-renderer"
+    , "ytd-promoted-sparkles-text-search-renderer", "darla", "my-adsLREC4"
+    , "sb_add", "my-adsLREC3-fallback"
+    ,"uEierd", "sb_rel_my-adsLREC3-iframe"]
+    
+    for(let i = 0; i < lads.length; i++){
+    
+        ads=document.getElementsByClassName(lads[i])
+        for(let i = 0; i < ads.length; i++){
+        console.log(ads[i])
+        ads[i].remove(ads[i])
+        }
+        
+    }}
